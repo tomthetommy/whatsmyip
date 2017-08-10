cd60b90bb7a365eb90f1b2b8798ea63d4ea3c58e
tom@tomalexanderhughes.com <tom@tomalexanderhughes.com>
2017-08-10 13:32:23 +0100

    added line

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+
 $ipAddress = $_SERVER['REMOTE_ADDR'];
 if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
     $ipAddress = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
